fd559361abf19c45d449d53566f0ef0b6c59682f
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2025-05-15 17:26:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,132 @@
-console.clear();
+src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"
 
-CustomEase.create("swish", "M0,0 C0,0 -0.097,0.589 0.1,0.589 0.3,0.589 0.075,0 0.212,0 0.344,0 0.106,0.9 0.354,0.9 0.508,0.9 0.319,0 0.443,0 0.601,0 0.423,0.806 0.57,0.806 0.651,0.806 0.529,0.01 0.717,0.01 0.904,0.01 0.811,1 0.917,1 1.002,1 1,0 1,0 ");
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.clear();
 
-gsap.registerPlugin(DrawSVGPlugin, CustomEase, CustomBounce);
+  // Function maintains aspect ratio and distance
+  // between Splash and slogan image
+  
+  const svg = document.getElementById("splash_svg");
+  // console.log(`const svg, ${svg}!`)
+  const slogan = document.getElementById("splash_slogan_img");
+  // console.log(`const slogan, ${slogan}!`)
+  function updateImageSize() {
+    const svgWidth = svg.clientWidth;
+     // console.log(`const svgWidth, ${svgWidth}!`)
+    const svgHeight = svg.clientHeight;
+    // console.log(`const svgHeight, ${svgHeight}!`)
+    const ratio = 0.425;
+     // console.log(`const ratio, ${svg}!`)
+    const resultWidth = svgWidth * ratio;
+     // console.log(`const resultWidth, ${resultWidth}!`)
+    const resultHeight = resultWidth * 0.175714286;
+    // console.log(`const resultHeight, ${resultHeight}!`)
+    slogan.style.width = `${resultWidth}px`;
+    // console.log(`const resultWidth, ${slogan.style.width}!`)
+    slogan.style.height = `${resultHeight}px`;
+    // console.log(`const resultWidth, ${resultHeight}!`)
 
-const tl = gsap.timeline({});
-// onComplete: myFunction resolve to home
+    // console.log(" Function slogan.style.width:")
+    // console.log(slogan.style.width);
+  }
+  updateImageSize();
 
-tl.set(".circle", {opacity:0});
+  // updates image size when browser size changes.
+  window.addEventListener("resize", updateImageSize);
 
-tl.from(".st0", {
-  drawSVG: 0,
-  stagger: 0.20,
-  ease: "power1.inOut",  
+// Wrapper function: GSAP match media
+  
+gsap.set("#dot", {opacity:1, xPercent: -50, yPercent: -50, transformOrigin: "50% 50%" }, 0);
+  
+gsap.from(".st0", {
+  duration: .25,
+  drawSVG: 0, 
+  stagger: .25,
 });
 
-tl.to(".circle", {opacity: 1}, ">");
-// '>' insert at the END of the previous animation
+// Reegister plugins  
+gsap.registerPlugin(DrawSVGPlugin,
+CustomBounce, CustomEase, MotionPathPlugin, MotionPathHelper)
 
-tl.to(".circle", {delay: 0.1, duration: 2, y: -500, strength: .7, squash: 4, ease: "swish" }, ">");
+// declare variables
+const tl = gsap.timeline();
+  
+tl.set("#dot", {x:0, y:0});
+  
+CustomBounce.create('myBounce', {
+strength:0.6,
+squash:4,
+endAtStart: false,
+squashID: "myBounce-squash"                 
+})
+
+tl.addLabel("Part I")
+  .to("#dot", { y: 400, duration: 2, ease: "myBounce" })
+  .to("#dot", { rotation: 15, duration: 0.2, yoyo: true, repeat: 5, ease: "sine.inOut" }, 0)
+  .to("#dot", {rotation: "+=25", x: 25, duration: 1, ease: "power1.in" }, 0)
+  .to("#dot", {duration: 2.2, scaleX: 1.6, scaleY: 0.4, ease: "myBounce-squash"},0)
+   
+tl.addLabel("Part II")
+  .to("#dot", {duration: 2,scaleX: 1.4, scaleY: 0.6, ease: "myBounce-squash", transformOrigin: "center bottom"}, 0)
+  .to("#dot", { duration: 2,
+  motionPath: {
+    path: "M24.14,403.18299 C59.233,395.89299 -2.013,460.12399 22.775,616.90599 25.34225,633.14274 112.257,443.31199 122.096,367.81999 125.962,338.14799 115.543,497.54699 123.296,497.54699",
+    align: "self",
+    alignOrigin: [0.5, 0.5],
+    autoRotate:true,
+    selected: true,
+  },
+    ease: "myBounce",
+});
+ 
+tl.addLabel("Part III")
+  .to("#dot", {y: 780, duration: 2, ease: "myBounce" },"-=.5")
+  .to("#dot", {duration: 2,scaleX: 1.7, scaleY: 0.8, ease: "myBounce-squash", transformOrigin: "center bottom"}, 0)
+     
+tl.addLabel("Part IV")
+  .to("#dot", {duration: 2, scaleX: 1.4, scaleY: 0.6, ease:"myBounce-squash", transformOrigin: "center bottom"})
+  .to("#dot", {duration: 2, ease: "bounce.in", 
+      motionPath: {
+        path: [
+          { x: 65, y: 780 },
+          { x: 165, y: 250 },
+          { x: 282, y: 830 }
+            ],
+         curviness: 3
+}});
+  
+  <!-- try resizeObserver to keep slogan centered at all times -->
+  
+  const svg_cont = document.getElementById('svg_container');
+  function setSloganPosition(xPercent, yPercent) {
+    slogan.style.left = `${xPercent}%`;
+    slogan.style.top = `${yPercent}%`;
+ }
+
+setSloganPosition(30, 99);
+
+window.addEventListener('resize', () => {
+    // Get the current percentages
+    const xPercent = parseFloat(slogan.style.left);
+    const yPercent = parseFloat(slogan.style.top);
+    
+    // Update child element position
+    setSloganPosition(30, 99);
+});
+
+  tl.addLabel("Part V")
+    .to("#dot", { duration: 1, r: 8, fill:"#D6B807" })
+    .to("#dot", .3, {scale: 3, opacity: 0, ease: "power4.out",
+  onComplete: () => tl.set("#dot", { scale: 2, opacity: 1 })
+                    });
+  
+CustomEase.getSVGData('myBounce', {path:"#bounce", width:1920, height:780});
+
+CustomEase.getSVGData('myBounce-squash', {path:"#squash", width:1920, height:780});
+
+CustomEase.getSVGData('dot', {path:"#dot", width:1920, height:830});
+
+GSDevTools.create({animation:tl});
+
+// MotionPathHelper.create("#dot",)
+});
